57b0ff7050f856006566eb2aeeab97673983caf9
bayuswara <54299369+bayuswara@users.noreply.github.com>
2020-03-03 19:34:57 +0700

    Add files via upload

---
+++ latihan5.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Variabel antar Halaman </title>
+</head>
+<body>
+<form action = "variabelhasil.php" method="POST">
+Nama : <input type="text" name="nama"> <br>
+Email : <input type = "text" name ="email"> <br>
+<input type = "submit" name ="submit" value = "Tampilkan">
+</form>
+</body>
+</html>
